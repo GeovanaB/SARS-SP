7c2c2ebd7f2a93667def9abf61aff0228d2eb54b
Geovana Bettero <geovana23021@ilum.cnpem.br>
2025-11-15 09:56:02 -0300

    Modelo CatBoost

---
+++ Modelos/catboost/caso4/job-catboost-4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -n 10
+#SBATCH --ntasks-per-node=10
+#SBATCH -p batch-AMD
+#SBATCH --time=900:00:00
+
+source ~/.bashrc
+conda activate tcc_darts
+
+python -u catboost-temp-optu-vali-4.py > catboost-temp-optu-vali-4.out
