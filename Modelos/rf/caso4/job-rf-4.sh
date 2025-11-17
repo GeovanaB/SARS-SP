8bff244696b7563c51f8274a8e21d95106edd174
Geovana Bettero <geovana23021@ilum.cnpem.br>
2025-11-16 21:28:19 -0300

    Modelo RF certinho

---
+++ Modelos/rf/caso4/job-rf-4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -n 5
+#SBATCH --ntasks-per-node=5
+#SBATCH -p batch-AMD
+#SBATCH --time=900:00:00
+
+source ~/.bashrc
+conda activate jax_gpu
+
+python -u rf-temp-optu-vali-4.py > rf-temp-optu-vali-4.out
