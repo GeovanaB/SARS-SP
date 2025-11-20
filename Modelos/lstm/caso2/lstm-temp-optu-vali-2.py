61cc8358a4efd6e09e751ab0184f062454e62040
Geovana Bettero <geovana23021@ilum.cnpem.br>
2025-11-19 21:39:20 -0300

    Modelos que rodaram até agora

---
+++ Modelos/lstm/caso2/lstm-temp-optu-vali-2.py
@@ -0,0 +1,167 @@
+import pandas as pd
+import numpy as np
+import pickle
+from optuna import create_study, Trial
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import KFold
+from darts.models import BlockRNNModel
+from darts import TimeSeries
+import torch
+
+#Constantes
+TRIALS = 400
+STUDY_NAME = "lstm-temporal-2"
+NUM_FOLDS = 10
+SEMENTE_ALEATORIA = 352
+
+
+# Ler dados
+df = pd.read_csv("../../../dados/Dataset-REAL-OFICIAL-2.csv", sep=',')
+
+#Separar dados de treino e teste
+
+df = df.drop(columns = ['Unnamed: 0'])
+#df = df.drop(columns = ['Unnamed: 0.1'])
+
+target = 'Casos Diários'
+features = list(df.columns)
+features.remove(target)
+features.remove("Data") 
+
+# Separando target e covariaveis, treino e teste
+tamanho_treino = round(len(df) * 0.9)
+# treino = df.loc[0:tamanho_treino]
+# teste = df.loc[tamanho_treino + 1: len(df)]
+
+series_target = TimeSeries.from_dataframe(df, time_col='Data', value_cols=[target], freq = 'D')
+series_covariates = TimeSeries.from_dataframe(df, time_col='Data', value_cols=features, freq = 'D')
+
+target_treino = series_target[0:tamanho_treino]
+target_teste = series_target[tamanho_treino + 1:len(series_target)]
+
+covariates_treino = series_covariates[0:tamanho_treino]
+covariates_teste = series_covariates[tamanho_treino +1 : len(series_covariates)]
+
+
+# Funcao p/ validacao cruzada
+def val_cruzada(modelo, folds, target_treino, covariates):
+    total_len = len(target_treino)
+
+    train_ratio = 0.9
+    test_ratio = 0.1
+
+    train_len = round((total_len * train_ratio) / folds)
+    test_len = round((total_len * test_ratio) / folds)
+
+    if train_len < 1 or test_len < 1:
+        print(f"Erro: serie muito curta ({total_len}) para {folds} folds com proporcao 90/10.")
+        return
+
+    maes = []
+
+    for i in range(folds):
+        start_idx = i * (train_len + test_len)
+        train_end_idx = start_idx + train_len
+        test_end_idx = train_end_idx + test_len
+
+        if i == folds - 1:
+            test_end_idx = total_len
+
+        train = target_treino[start_idx:train_end_idx]
+        test = target_treino[train_end_idx:test_end_idx]
+        covariates_treino = covariates[start_idx:train_end_idx]
+
+        modelo.fit(train, past_covariates = covariates_treino)
+
+        pred = modelo.predict(len(test), past_covariates = covariates)
+        mae = mean_absolute_error(pred.values(), test.values())
+        maes.append(mae)
+
+        if test_end_idx >= total_len:
+            break
+
+    return sum(maes)/len(maes), maes 
+ 
+
+
+#Otimização de hiperparâmetros
+def cria_instancia_modelo(trial):
+
+    parametros = {
+        "hidden_dim": trial.suggest_int("hidden_dim", 32, 256),
+	    "n_rnn_layers": trial.suggest_int("n_rnn_layers",  1, 4),
+        "dropout": trial.suggest_float("dropout", 0.1, 0.5),
+        "input_chunk_length": trial.suggest_int("input_chunk_length", 10, 60),
+        "output_chunk_length": trial.suggest_int("output_chunk_length", 1, 7),
+        "batch_size": trial.suggest_categorical("batch_size", [16,32,64]),
+        "model" :"LSTM",
+        "random_state": SEMENTE_ALEATORIA,
+        "loss_fn" : torch.nn.SmoothL1Loss(),
+        "log_tensorboard": True
+    }
+
+    lr = trial.suggest_float("lr", 1e-4, 5e-3, log=True)
+
+    model = BlockRNNModel(**parametros, optimizer_kwargs = {"lr": lr}, pl_trainer_kwargs = {"accelerator": "gpu"})
+
+    return model
+
+def funcao_objetivo(
+    trial,
+    X,
+    y
+):
+
+    modelo = cria_instancia_modelo(trial)
+    valor, folds = val_cruzada(modelo, NUM_FOLDS, X, y) 
+
+    return valor
+
+study = create_study(
+    study_name=STUDY_NAME,
+    storage=f"sqlite:///hptuning/{STUDY_NAME}.db",
+    direction="minimize",
+    load_if_exists=True,
+)
+
+df_op = study.trials_dataframe()
+if len(df_op) == 0:
+    num_trials = TRIALS
+else:
+    num_trials = TRIALS - len(df_op.loc[(df_op["state"] == "COMPLETE")])
+ 
+def funcao_objetivo_parcial(trial):
+    return funcao_objetivo(trial, target_treino, covariates_treino)
+
+study.optimize(funcao_objetivo_parcial, n_trials=num_trials)
+
+trialdf = study.trials_dataframe()
+trialdf.to_csv(f"hptuning/{STUDY_NAME}.csv", index=False)
+
+melhor_trial = study.best_trial
+
+print(f"Número do melhor trial: {melhor_trial.number}")
+print(f"Parâmetros do melhor trial: {melhor_trial.params}")
+
+modelo_final = cria_instancia_modelo(melhor_trial)
+
+# Fzendo a validação cruzada de novo só p/ pegar os 10 kfolds
+melhor_valor, melhor_folds = val_cruzada(modelo_final, NUM_FOLDS, target_treino, covariates_treino)
+
+modelo_final = cria_instancia_modelo(melhor_trial)
+modelo_final.fit(series = target_treino, past_covariates = covariates_treino)
+
+y_verdadeiro = target_teste.values()
+y_previsao = modelo_final.predict(n = len(target_teste),past_covariates = series_covariates)
+
+MAE_teste = mean_absolute_error(y_verdadeiro, y_previsao.values())
+
+print(f"Mae no teste:{MAE_teste}.")
+
+# salvando modelo
+with open(f"{STUDY_NAME}.pkl", "wb") as arquivo:
+    pickle.dump(modelo_final, arquivo)
+    
+# salvando kfolds  
+with open(f"{STUDY_NAME}_FOLDS.pkl", "wb") as arquivo:
+    pickle.dump(melhor_folds, arquivo)
